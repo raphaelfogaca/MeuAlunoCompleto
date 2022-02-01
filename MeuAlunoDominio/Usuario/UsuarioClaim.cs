9457097372ee4b708a0e208d02b880d4e6992d49
Raphael <raphael15br@gmail.com>
2022-01-31 23:42:05 -0300

    jwt implementado

---
+++ MeuAlunoDominio/Usuario/UsuarioClaim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuAlunoDominio
+{
+    public class UsuarioClaim 
+    {
+        public string ClaimName { get; set; }
+        public string ClaimValue { get; set;}
+    }
+}
--- MeuAlunoDominio/Usuario/UsuarioTokenModelo.cs
+++ MeuAlunoDominio/Usuario/UsuarioTokenModelo.cs
@@ -1,21 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MeuAlunoDominio
 {
-    public class UsuarioTokenModelo
+    public class UsuarioTokenModelo : IdentityUser
     {
-        public int Id { get; set; }
         public string Login { get; set; }
         public int PessoaId { get; set; }
         public string PessoaNome { get; set; }
-        public string Email { get; set; }
         public int EmpresaId { get; set; }
         public string EmpresaNome { get; set; }
         public List<Empresa> Empresa { get; set; }
-        public int TipoUsuario { get; set; }
-        public DateTime ExpirationDate { get; set; }
+        public int TipoUsuario { get; set; }        
+        public int ExpirationTime { get; set; }
+        public string Emissor { get; set; }
+        public string Secret { get; set; }
+        public string ValidoEm { get; set; }
 
     }
 }
+
+
